185e4b79ea410f3dd1bb54e1b664924bd26563f9
yakoubboum <yakoubboum7@gmail.com>
2024-02-19 09:49:19 +0100

    first

--- routes/backend.php
+++ routes/backend.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/newdashboard', [DashboardController::class, 'index']);
