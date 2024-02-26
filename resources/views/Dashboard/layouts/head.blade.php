7473d90fa4ac5bb3227688765995e765e3692587
yakoubboum <yakoubboum7@gmail.com>
2024-02-26 08:36:06 +0100

    setup translation

--- resources/views/Dashboard/layouts/head.blade.php
+++ resources/views/Dashboard/layouts/head.blade.php
@@ -1,6 +1,8 @@
 <!-- Title -->
 <title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
 <!-- Favicon -->
+
+@if (App::getLocale()=='ar')
 <link rel="icon" href="{{URL::asset('Dashboard/img/brand/favicon.png')}}" type="image/x-icon"/>
 <!-- Icons css -->
 <link href="{{URL::asset('Dashboard/css/icons.css')}}" rel="stylesheet">
@@ -10,10 +12,31 @@
 <link href="{{URL::asset('Dashboard/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
 <!-- Sidemenu css -->
 <link rel="stylesheet" href="{{URL::asset('Dashboard/css-rtl/sidemenu.css')}}">
-@yield('css')
+
 <!--- Style css -->
 <link href="{{URL::asset('Dashboard/css-rtl/style.css')}}" rel="stylesheet">
 <!--- Dark-mode css -->
 <link href="{{URL::asset('Dashboard/css-rtl/style-dark.css')}}" rel="stylesheet">
 <!---Skinmodes css-->
 <link href="{{URL::asset('Dashboard/css-rtl/skin-modes.css')}}" rel="stylesheet">
+@else
+<link rel="icon" href="{{URL::asset('Dashboard/img/brand/favicon.png')}}" type="image/x-icon"/>
+<!-- Icons css -->
+<link href="{{URL::asset('Dashboard/css/icons.css')}}" rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href="{{URL::asset('Dashboard/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<!--  Right-sidemenu css -->
+<link href="{{URL::asset('Dashboard/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{URL::asset('Dashboard/css/sidemenu.css')}}">
+@yield('css')
+<!-- Maps css -->
+<link href="{{URL::asset('Dashboard/plugins/jqvmap/jqvmap.min.css')}}" rel="stylesheet">
+<!-- style css -->
+<link href="{{URL::asset('Dashboard/css/style.css')}}" rel="stylesheet">
+<link href="{{URL::asset('Dashboard/css/style-dark.css')}}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{URL::asset('Dashboard/css/skin-modes.css')}}" rel="stylesheet" />
+@endif
+
+
--- routes/backend.php
+++ routes/backend.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard/user', function () {
-    return view('Dashboard.User.dashboard');
-})->middleware(['auth:web', 'verified'])->name('dashboard.user');
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+
+        Route::get('/dashboard/user', function () {
+            return view('Dashboard.User.dashboard');
+        })->middleware(['auth:web', 'verified'])->name('dashboard.user');
 
 
-Route::get('/dashboard/admin', function () {
-    return view('Dashboard.Admin.dashboard');
-})->middleware(['auth:admin', 'verified'])->name('dashboard.admin');
+        Route::get('/dashboard/admin', function () {
+            return view('Dashboard.Admin.dashboard');
+        })->middleware(['auth:admin', 'verified'])->name('dashboard.admin');
 
 
-require __DIR__.'/auth.php';
+        require __DIR__ . '/auth.php';
+    }
+);
